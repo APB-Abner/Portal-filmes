949bd7c1904971bb45efafaf4eb53fc6e0474e03
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2024-09-10 20:32:19 -0300

    Add Page404 component

---
+++ src/pages/Page404.jsx
@@ -0,0 +1,10 @@
+function Page404() {
+    return ( 
+        <div>
+            <h2>404 - Página não encontrada</h2>
+            <p>Está está em desenvolvimento ou não exite, entre em contato!</p>
+        </div>
+     );
+}
+
+export default Page404;
