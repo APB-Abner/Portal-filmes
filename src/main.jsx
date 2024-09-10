4cb283e0505c7be3e1e2790b7aa3df1287fce633
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2024-09-10 20:32:12 -0300

    Refactor main.jsx to implement routing and add new pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import GenreListPage from './pages/GenreListPage.jsx'
+import MovieListPage from './pages/MovieListPage.jsx'
+import MovieDetailPage from './pages/MovieDetailPage.jsx'
+import MoviesByGenrePage from './pages/MoviesByGenrePage.jsx'
+import PageNotFound from './pages/Page404.jsx'
 
-{/* Deve ter as rotas:
-  home
-  filmes
-  Detalhes do Filme
-  Lista de gêneros
-  Filmes por gênero
-  Page Not Found
-  */}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'genre', element: <GenreListPage /> },
+      { path: 'movie', element: <MovieListPage /> },
+      { path: 'movie/:id', element: <MovieDetailPage /> },
+      { path: 'movie/:ge', element: <MoviesByGenrePage /> },
+      { path: '*', element: <PageNotFound /> },
+    ]
+  }
+])
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
