349eafbaad507c9041aa02bd278ff86f095974bb
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2024-09-17 21:25:40 -0300

    Refactor MovieCard component to include movie details and a link to learn more

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import CardContainer from "../components/CardContainer";
+import MovieCard from "../components/MovieCard";
+import movies from "../data/movies.json";
 
 export default function Home(){
+    console.log(movies)
     return(
         <>
-        <CardContainer>
-            <h1> Filmes</h1>
+        <CardContainer titulo="Filmes Antigos">
+            {movies
+            .filter( movie => movie.ano_lancamento< 2000)
+            .map(movie => (
+                <MovieCard key={movie.id} {...movie} />
+
+            ))}
         </CardContainer>
         </>
     )
